68e4bee9aa097f6260241f8d888090b333479313
Jason Solano <jasonsolanof@gmail.com>
2024-08-30 23:46:44 -0600

    Add Put and Delete endpoints

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/web/TourRatingController.java
@@ -1,15 +1,12 @@
 package com.example.explorecalijpa.web;
 
+import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
+import com.example.explorecalijpa.model.TourRating;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.explorecalijpa.business.TourRatingService;
 
@@ -40,6 +37,30 @@ public class TourRatingController {
   public void createTourRating(@PathVariable(value = "tourId") int tourId,
       @RequestBody @Valid RatingDto ratingDto) {
     tourRatingService.createNew(tourId, ratingDto.getCustomerId(), ratingDto.getScore(), ratingDto.getComment());
+  }
+
+  @GetMapping
+  public List<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId) {
+    List<TourRating> tourRatings = tourRatingService.lookupRatings(tourId);
+    return tourRatings.stream().map(RatingDto::new).toList();
+  }
+
+  @GetMapping("/average")
+  public Map<String, Double> getAverage(@PathVariable(value="tourId") int tourId) {
+    return Map.of("average", tourRatingService.getAverageScore(tourId));
+  }
+
+  @PutMapping
+  public RatingDto updateTourRating(@PathVariable(value = "tourId") int tourId, @RequestBody @Valid RatingDto ratingDto)
+  {
+    return new RatingDto(tourRatingService.update(tourId, ratingDto.getCustomerId(), ratingDto.getScore(),
+            ratingDto.getComment()));
+  }
+
+  @DeleteMapping("{customerId}")
+  public void deleteTourRating(@PathVariable(value = "tourId") int tourId, @PathVariable(value = "customerId")
+                               int customerId) {
+    tourRatingService.delete(tourId, customerId);
   }
 
   @ExceptionHandler(NoSuchElementException.class)
